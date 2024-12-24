8d9464ecf0aa99906d7587c9977ee50524b712e1
BeamedRunsYou <bluestripz963@gmail.com>
2024-12-24 00:44:35 -0800

    Create discord.py

---
+++ discord.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+# Intents are required for the bot to have access to certain events
+intents = discord.Intents.default()
+intents.messages = True
+
+# Create a bot instance with the specified prefix and intents
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+# Replace 'YOUR_BOT_TOKEN' with your bot's token
+bot.run('YOUR_BOT_TOKEN')
